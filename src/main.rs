5462de03037d2279c2ba081150489b26ef99b78f
Potat05 <williamkoerber6@gmail.com>
2023-09-16 19:35:31 -0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,67 @@
 use std::{fs::File, io::Write, path::PathBuf};
 
 use base64::{engine::general_purpose, Engine};
-use image::EncodableLayout;
+use image::{EncodableLayout, DynamicImage};
 use clap::{Parser, ValueEnum};
 
 
 
-#[derive(ValueEnum, Debug, Clone)]
+#[derive(ValueEnum, Debug, Clone, Copy)]
 enum EncodingMethod {
     BC1 = 4,
     RGB888 = 5
 }
 
-// // TODO: Get this to work
-// #[derive(Args, Debug, Clone)]
-// struct Resize {
-//     width: u32,
-//     height: u32,
-// }
+impl EncodingMethod {
+
+    pub fn byte_size(self, width: usize, height: usize) -> usize {
+        match self {
+            EncodingMethod::BC1 => texpresso::Format::Bc1.compressed_size(width, height),
+            EncodingMethod::RGB888 => width * height * 3,
+        }
+    }
+
+    pub fn encode(self, img: &DynamicImage) -> Vec<u8> {
+
+        let width: usize = img.width().try_into().unwrap();
+        let height: usize = img.height().try_into().unwrap();
+
+        match self {
+            EncodingMethod::BC1 => {
+
+                let size = texpresso::Format::Bc1.compressed_size(width, height);
+                let mut encoded = vec![0u8; size];
+
+                texpresso::Format::Bc1.compress(
+                    img.to_rgba8().as_bytes(),
+                    width,
+                    height,
+                    texpresso::Params {
+                        algorithm: texpresso::Algorithm::IterativeClusterFit,
+                        weights: [ 0.2126, 0.7152, 0.0722 ],
+                        weigh_colour_by_alpha: false
+                    },
+                    &mut encoded
+                );
+
+                encoded
+
+            },
+            EncodingMethod::RGB888 => {
+
+                img.to_rgb8().as_bytes().to_vec()
+
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
 
 /// Encode an image that gmod_e2_image.txt can decode in-game.
 #[derive(Parser, Debug, Clone)]
@@ -32,10 +76,6 @@ struct Command {
     #[arg(short, long, value_enum, default_value_t = EncodingMethod::BC1)]
     encoding: EncodingMethod,
 
-    // /// Resize image
-    // #[arg(short, long)] 
-    // resize: Option<Resize>,
-
 }
 
 
@@ -47,6 +87,8 @@ fn main() -> std::io::Result<()> {
 
 
 
+    println!("Loading image {:?}", cli.img_path);
+
     let img = image::open(cli.img_path).unwrap();
 
     if img.width() > 0xFFFF || img.height() > 0xFFFF {
@@ -57,55 +99,38 @@ fn main() -> std::io::Result<()> {
 
 
 
-    // if Option::is_some(&cli.resize) {
-    //     let resize = cli.resize.unwrap();
-    //     // I don't know what filtering method to use, I just use Lanczos3 because it sounds the most fancy.
-    //     img = img.resize(resize.width, resize.height, image::imageops::FilterType::Lanczos3);
-    // }
-    let rgba8888 = img.into_rgba8();
+    // // I don't know what filtering method to use, I just use Lanczos3 because it sounds the most fancy.
+    // img = img.resize(resize.width, resize.height, image::imageops::FilterType::Lanczos3);
 
 
-
-    let encoded = match cli.encoding {
-        EncodingMethod::BC1 => {
-            let size = texpresso::Format::Bc1.compressed_size(rgba8888.width().try_into().unwrap(), rgba8888.height().try_into().unwrap());
-            let mut buf = vec![0u8; size];
-            texpresso::Format::Bc1.compress(
-                rgba8888.as_bytes(),
-                rgba8888.width().try_into().unwrap(),
-                rgba8888.height().try_into().unwrap(),
-                texpresso::Params {
-                    algorithm: texpresso::Algorithm::IterativeClusterFit,
-                    weights: [ 0.2126, 0.7152, 0.0722 ],
-                    weigh_colour_by_alpha: false
-                },
-                &mut buf
-            );
-            buf
-        },
-        EncodingMethod::RGB888 => {
-            let buf = Vec::from(rgba8888.as_bytes());
-            buf
-        },
-    };
 
     let header = vec![
-        u8::from((rgba8888.width() & 0xFF) as u8),
-        u8::from(((rgba8888.width() >> 8) & 0xFF) as u8),
-        u8::from((rgba8888.height() & 0xFF) as u8),
-        u8::from(((rgba8888.height() >> 8) & 0xFF) as u8),
+        u8::from((img.width() & 0xFF) as u8),
+        u8::from(((img.width() >> 8) & 0xFF) as u8),
+        u8::from((img.height() & 0xFF) as u8),
+        u8::from(((img.height() >> 8) & 0xFF) as u8),
         u8::from(cli.encoding as u8),
     ];
 
 
 
-    static OUTPUTFILE: &str = "output.txt";
+    println!("Encoding image to {:?} format", cli.encoding);
 
-    println!("Output to file \"{}\"", OUTPUTFILE);
+    let encoded = cli.encoding.encode(&img);
 
-    let mut file = File::create(OUTPUTFILE)?;
+
+
+    let output_file: PathBuf = PathBuf::from("output.txt");
+
+    println!("Writing to file {:?}", output_file);
+
+    let mut file = File::create(output_file)?;
 
     let base64_stream = general_purpose::STANDARD.encode([ header, encoded ].concat());
+
+    if base64_stream.len() > 200_000 {
+        println!("WARNING: Image stream is large, pasting into e2 may take a while.");
+    }
 
     file.write_all(format!("Base64Stream = \"{}\"", base64_stream).as_bytes())?;
 
